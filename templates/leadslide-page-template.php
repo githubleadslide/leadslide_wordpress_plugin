619a4e9deb75b87c469602efb3830a311c76c01b
Martyn <martyn@justunravel.com>
2023-10-08 15:05:49 +0100

    Latest Version

--- templates/leadslide-page-template.php
+++ templates/leadslide-page-template.php
@@ -2,15 +2,37 @@
 /*
 Template Name: Leadslide Page
 */
-global $BASE_VIEW_URL;
-$campaign_id = get_post_meta(get_the_ID(), 'campaign_id', true);
-$publish_api_key = get_post_meta(get_the_ID(), 'publish_api_key', true);
+global $BASE_LEADSLIDE_VIEW_URL;
+$leadslide_campaign_id = get_post_meta(get_the_ID(), 'campaign_id', true);
+$leadslide_publish_api_key = get_post_meta(get_the_ID(), 'publish_api_key', true);
 
-if (empty($campaign_id) || empty($publish_api_key)) {
-    echo 'The custom fields are not set properly. Please set them.';
+if (empty($leadslide_campaign_id) || empty($leadslide_publish_api_key)) {
+    echo 'Campaign not installed properly, please contact leadslide support.';
     return;
 }
 
 ?>
 
-<iframe src="<?php echo $BASE_VIEW_URL; ?>?cid=<?php echo $campaign_id; ?>&pak=<?php echo $publish_api_key; ?>" frameborder="0" width="100%" style="height:100vh;"></iframe>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+        }
+    </style>
+    <title>
+        <?php echo get_the_title(); ?>
+    </title>
+</head>
+</html>
+<body>
+<iframe
+        id="leadslidePageFrame"
+        src="<?php echo $BASE_LEADSLIDE_VIEW_URL; ?>?cid=<?php echo $leadslide_campaign_id; ?>&pak=<?php echo $leadslide_publish_api_key; ?>" scrolling="yes" frameborder="0" width="100%" style="min-height:100vh;"></iframe>
+</body>
+</html>
+
+
+
